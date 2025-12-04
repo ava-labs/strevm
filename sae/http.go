255a9ca60a3d3f2019ef8c5db8a9f94c82706003
Arran Schlosberg <me@arranschlosberg.com>
2025-12-04 10:47:59 +0000

    fix: satisfy updates to `common.VM`

--- sae/http.go
+++ sae/http.go
@@ -9,8 +9,15 @@ import (
 )
 
 // CreateHandlers returns all VM-specific HTTP handlers to be exposed by the
-// node, keyed by path.
+// node, keyed by extension.
 func (vm *VM) CreateHandlers(context.Context) (map[string]http.Handler, error) {
+	return nil, errUnimplemented
+}
+
+// NewHTTPHandler returns the HTTP handler that will be invoked if a client
+// passes this VM's chain ID via the routing header described in the [common.VM]
+// documentation for this method.
+func (vm *VM) NewHTTPHandler(context.Context) (http.Handler, error) {
 	return nil, errUnimplemented
 }
 
