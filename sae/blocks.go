6df8f4a8b733c61c8c95aa0345ab30ced96c20f1
Stephen Buttolph <stephen@avalabs.org>
2025-12-23 15:58:18 -0500

    add more logs

--- sae/blocks.go
+++ sae/blocks.go
@@ -74,8 +74,8 @@ func (vm *VM) BuildBlock(ctx context.Context, bCtx *block.Context) (*blocks.Bloc
 	)
 }
 
-// Max time from current time allowed for blocks, before they're considered future blocks
-// and fail verification
+// Max time from current time allowed for blocks, before they're considered
+// future blocks and fail verification.
 const maxFutureBlockTime = 10 * time.Second
 
 var errExecutionLagging = errors.New("execution lagging for settlement")
@@ -120,26 +120,52 @@ func (vm *VM) buildBlock(
 		return nil, errExecutionLagging
 	}
 
+	log = log.With(
+		zap.Uint64("last_settled_height", lastSettled.Height()),
+		zap.Stringer("last_settled_hash", lastSettled.Hash()),
+	)
+
 	state, err := worstcase.NewState(vm.hooks, vm.exec.ChainConfig(), vm.exec.StateCache(), lastSettled)
 	if err != nil {
+		log.Warn("Settled state not available")
 		return nil, err
 	}
 
 	for _, b := range unsettledAncestry(parent, lastSettled.Height()) {
+		log := log.With(
+			zap.Uint64("block_height", b.Height()),
+			zap.Stringer("block_hash", b.Hash()),
+		)
 		if err := state.StartBlock(b.Header()); err != nil {
+			log.Warn("Could not start historical worst case calculation",
+				zap.Error(err),
+			)
 			return nil, fmt.Errorf("starting worst-case state for block %d: %v", b.Height(), err)
 		}
-		for _, tx := range b.Transactions() {
+		for i, tx := range b.Transactions() {
 			if err := state.ApplyTx(tx); err != nil {
+				log.Warn("Could not apply tx during historical worst case calculation",
+					zap.Int("tx_index", i),
+					zap.Stringer("tx_hash", tx.Hash()),
+					zap.Error(err),
+				)
 				return nil, fmt.Errorf("applying tx %#x in block %d to worst-case state: %v", tx.Hash(), b.Height(), err)
 			}
 		}
-		for _, op := range vm.hooks.EndOfBlockOps(b.EthBlock()) {
+		for i, op := range vm.hooks.EndOfBlockOps(b.EthBlock()) {
 			if err := state.Apply(op); err != nil {
+				log.Warn("Could not apply op during historical worst case calculation",
+					zap.Int("op_index", i),
+					zap.Stringer("op_id", op.ID),
+					zap.Error(err),
+				)
 				return nil, fmt.Errorf("applying op at end of block %d to worst-case state: %v", b.Height(), err)
 			}
 		}
 		if err := state.FinishBlock(); err != nil {
+			log.Warn("Could not finish historical worst case calculation",
+				zap.Error(err),
+			)
 			return nil, fmt.Errorf("finishing worst-case state for block %d: %v", b.Height(), err)
 		}
 	}
@@ -162,6 +188,9 @@ func (vm *VM) buildBlock(
 		// [types.Transaction].
 	}
 	if err := state.StartBlock(hdr); err != nil {
+		log.Warn("Could not start worst case block calculation",
+			zap.Error(err),
+		)
 		return nil, fmt.Errorf("starting worst-case state for new block: %v", err)
 	}
 
@@ -199,6 +228,9 @@ func (vm *VM) buildBlock(
 	// Although we never interact with the worst-case state after this point, we
 	// still mark the block as finished to align with normal execution.
 	if err := state.FinishBlock(); err != nil {
+		log.Warn("Could not finish worst case block calculation",
+			zap.Error(err),
+		)
 		return nil, fmt.Errorf("finishing worst-case state for new block: %v", err)
 	}
 
--- sae/vm_test.go
+++ sae/vm_test.go
@@ -270,3 +270,42 @@ func TestSyntacticBlockChecks(t *testing.T) {
 		})
 	}
 }
+
+func TestSemanticBlockChecks(t *testing.T) {
+	ctx, sut := newSUT(t, 0)
+
+	const now = 1e6
+	sut.rawVM.config.Now = func() time.Time {
+		return time.Unix(now, 0)
+	}
+
+	tests := []struct {
+		name    string
+		header  *types.Header
+		wantErr error
+	}{
+		{
+			name: "block_height_overflow_protection",
+			header: &types.Header{
+				Number: new(big.Int).Lsh(big.NewInt(1), 64),
+			},
+			wantErr: errBlockHeightNotUint64,
+		},
+		{
+			name: "block_time_overflow_protection",
+			header: &types.Header{
+				Number: big.NewInt(1),
+				Time:   now + maxBlockFutureSeconds + 1,
+			},
+			wantErr: errBlockTooFarInFuture,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := blockstest.NewBlock(t, types.NewBlockWithHeader(tt.header), nil, nil)
+			_, err := sut.ParseBlock(ctx, b.Bytes())
+			assert.ErrorIs(t, err, tt.wantErr, "ParseBlock(#%v @ time %v) when stubbed time is %d", tt.header.Number, tt.header.Time, uint64(now))
+		})
+	}
+}
