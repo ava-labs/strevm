f1dcd607eecd2718eac583bbc7b68695352fcc9d
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-01-28 11:44:34 -0500

    fix: disable snapshot for integration test

--- blocks/blockstest/blocks_test.go
+++ blocks/blockstest/blocks_test.go
@@ -45,7 +45,10 @@ func TestIntegration(t *testing.T) {
 	// tested with the builder, then we would have a circular argument for
 	// correctness.
 	bc, err := core.NewBlockChain(
-		db, nil,
+		db,
+		&core.CacheConfig{
+			SnapshotNoBuild: true, // Disable snapshot to avoid warning in tests
+		},
 		&core.Genesis{
 			Config: config,
 			Alloc:  alloc,
