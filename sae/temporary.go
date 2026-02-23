4e04f6d3de941cba33258d10d2fed5149916c782
Jonathan Oppenheimer <147infiniti@gmail.com>
2026-02-23 12:47:22 -0500

    chore: add tracers stubs (#222)
    While all other stubs were present in `temporary.go`, these two methods
    were missing. This sets up the config, and makes clear all the RPC
    methods that still need to be implemented.

--- sae/temporary.go
+++ sae/temporary.go
@@ -12,7 +12,11 @@ import (
 	"math/big"
 
 	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/core"
+	"github.com/ava-labs/libevm/core/state"
 	"github.com/ava-labs/libevm/core/types"
+	"github.com/ava-labs/libevm/core/vm"
+	"github.com/ava-labs/libevm/eth/tracers"
 	"github.com/ava-labs/libevm/rpc"
 )
 
@@ -35,5 +39,13 @@ func (b *ethAPIBackend) PendingBlockAndReceipts() (*types.Block, types.Receipts)
 }
 
 func (b *ethAPIBackend) GetPoolNonce(context.Context, common.Address) (uint64, error) {
+	panic(errUnimplemented)
+}
+
+func (b *ethAPIBackend) StateAtBlock(ctx context.Context, block *types.Block, reexec uint64, base *state.StateDB, readOnly bool, preferDisk bool) (*state.StateDB, tracers.StateReleaseFunc, error) {
+	panic(errUnimplemented)
+}
+
+func (b *ethAPIBackend) StateAtTransaction(ctx context.Context, block *types.Block, txIndex int, reexec uint64) (*core.Message, vm.BlockContext, *state.StateDB, tracers.StateReleaseFunc, error) {
 	panic(errUnimplemented)
 }
