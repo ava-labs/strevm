819dd5ce518f58cb0783c9130c2bdddc72f25da9
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2026-03-10 14:42:13 +0200

    test(sae): rewrite trace tests per ARR4N's 604256a commit
    Adopt ARR4N's precise StructLog assertion approach:
    - Filter to the `LOG1` opcode at the exact program counter of `emit Deposit()` in the escrow
      bytecode, verifying stack contents match the event topic and memory locations.

--- sae/rpc_stateful_test.go
+++ sae/rpc_stateful_test.go
@@ -8,10 +8,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/arr4n/shed/testerr"
 	ethereum "github.com/ava-labs/libevm"
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/common/hexutil"
 	"github.com/ava-labs/libevm/core/types"
+	"github.com/ava-labs/libevm/core/vm"
 	"github.com/ava-labs/libevm/crypto"
 	"github.com/ava-labs/libevm/eth/tracers/logger"
 	"github.com/ava-labs/libevm/rpc"
@@ -25,14 +27,7 @@ import (
 	"github.com/ava-labs/strevm/saetest/escrow"
 )
 
-// txTraceResult mirrors the unexported tracers.txTraceResult.
-type txTraceResult struct {
-	TxHash common.Hash             `json:"txHash"`
-	Result *logger.ExecutionResult `json:"result,omitempty"`
-	Error  string                  `json:"error,omitempty"`
-}
-
-func TestTraceContractInteraction(t *testing.T) {
+func TestDebugTrace(t *testing.T) {
 	ctx, sut := newSUT(t, 1)
 
 	escrowAddr := crypto.CreateAddress(sut.wallet.Addresses()[0], 0)
@@ -55,46 +50,91 @@ func TestTraceContractInteraction(t *testing.T) {
 
 	b := sut.runConsensusLoop(t, deployTx, depositTx)
 	require.NoErrorf(t, b.WaitUntilExecuted(ctx), "%T.WaitUntilExecuted()", b)
+	require.Lenf(t, b.Receipts(), 2, "%T.Receipts()", b)
 	for _, r := range b.Receipts() {
 		require.Equalf(t, types.ReceiptStatusSuccessful, r.Status, "%T.Status", r)
 	}
 
-	deployReceipt := b.Receipts()[0]
-	depositReceipt := b.Receipts()[1]
+	// Specifying the entire trace would be excessive and uninformative so we
+	// select a precise location of an event associated with the deposit()
+	// function on the contract.
+	const logPC = 185
+	require.Equal(t, vm.LOG1, vm.OpCode(escrow.ByteCode()[logPC]), "Bad test setup; program counter LOG1 for `emit Deposit()`")
+	ignore := cmp.Options{
+		cmpopts.IgnoreSliceElements(func(r logger.StructLogRes) bool {
+			return r.Pc != logPC || r.Op != vm.LOG1.String()
+		}),
+		// Any precise amount of gas left at the time of OpCode execution would
+		// be copy-pasted from the test output.
+		cmpopts.IgnoreFields(logger.StructLogRes{}, "Gas", "GasCost"),
+	}
 
-	sut.testRPC(ctx, t, []rpcTest{
+	want := []struct {
+		TxHash common.Hash             `json:"txHash"`
+		Result *logger.ExecutionResult `json:"result"`
+		Error  string                  `json:"error"`
+	}{
 		{
-			method: "debug_traceTransaction",
-			args:   []any{depositTx.Hash()},
-			want: logger.ExecutionResult{
-				Gas:         depositReceipt.GasUsed,
-				Failed:      false,
-				ReturnValue: "",
-			},
-			extraCmpOpts: []cmp.Option{
-				cmpopts.IgnoreFields(logger.ExecutionResult{}, "StructLogs"),
+			TxHash: deployTx.Hash(),
+			Result: &logger.ExecutionResult{
+				Gas:         b.Receipts()[0].GasUsed,
+				ReturnValue: common.Bytes2Hex(escrow.ByteCode()),
+				StructLogs:  []logger.StructLogRes{},
 			},
 		},
 		{
-			method: "debug_traceBlockByNumber",
-			args:   []any{hexutil.Uint64(b.Height())},
-			want: []txTraceResult{
-				{TxHash: deployTx.Hash(), Result: &logger.ExecutionResult{
-					Gas:         deployReceipt.GasUsed,
-					Failed:      false,
-					ReturnValue: common.Bytes2Hex(escrow.ByteCode()),
+			TxHash: depositTx.Hash(),
+			Result: &logger.ExecutionResult{
+				Gas: b.Receipts()[1].GasUsed,
+				StructLogs: []logger.StructLogRes{{
+					Pc:    logPC,
+					Op:    vm.LOG1.String(),
+					Depth: 1,
+					Stack: ptrTo([]string{
+						escrow.DepositEvent(recv, uint256.NewInt(depositVal)).Topics[0].String(),
+						"0x40", "0x80", // arbitrary memory locations selected by Solidity
+					}),
 				}},
-				{TxHash: depositTx.Hash(), Result: &logger.ExecutionResult{
-					Gas:         depositReceipt.GasUsed,
-					Failed:      false,
-					ReturnValue: "",
-				}},
-			},
-			extraCmpOpts: []cmp.Option{
-				cmpopts.IgnoreFields(logger.ExecutionResult{}, "StructLogs"),
 			},
 		},
-	}...)
+	}
+
+	tests := []rpcTest{
+		{
+			method:       "debug_traceBlockByNumber",
+			args:         []any{hexutil.Uint64(b.NumberU64())},
+			want:         want,
+			extraCmpOpts: ignore,
+		},
+		{
+			method:       "debug_traceBlockByNumber",
+			args:         []any{rpc.LatestBlockNumber},
+			want:         want,
+			extraCmpOpts: ignore,
+		},
+		{
+			method:       "debug_traceBlockByHash",
+			args:         []any{b.Hash()},
+			want:         want,
+			extraCmpOpts: ignore,
+		},
+		{
+			method:  "debug_traceTransaction",
+			args:    []any{common.Hash{}},
+			wantErr: testerr.Contains("not found"),
+		},
+	}
+
+	for _, tx := range want {
+		tests = append(tests, rpcTest{
+			method:       "debug_traceTransaction",
+			args:         []any{tx.TxHash},
+			want:         *tx.Result,
+			extraCmpOpts: ignore,
+		})
+	}
+
+	sut.testRPC(ctx, t, tests...)
 }
 
 func TestEthCall(t *testing.T) {
