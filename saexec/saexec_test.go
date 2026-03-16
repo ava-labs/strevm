d94ee19daaa3d800a6dadc1534da6c3c2db771d8
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-03-16 12:11:10 -0400

    chore: fix formatting on comments

--- saexec/saexec_test.go
+++ saexec/saexec_test.go
@@ -9,6 +9,7 @@ import (
 	"math"
 	"math/big"
 	"math/rand/v2"
+	"os"
 	"slices"
 	"testing"
 
@@ -25,6 +26,7 @@ import (
 	"github.com/ava-labs/libevm/ethdb"
 	"github.com/ava-labs/libevm/libevm"
 	libevmhookstest "github.com/ava-labs/libevm/libevm/hookstest"
+	"github.com/ava-labs/libevm/log"
 	"github.com/ava-labs/libevm/params"
 	"github.com/ava-labs/libevm/triedb"
 	"github.com/google/go-cmp/cmp"
@@ -65,24 +67,12 @@ type SUT struct {
 	db     ethdb.Database
 }
 
-// newSUTOption configures [newSUT] behaviour.
-type newSUTOption func(*newSUTConfig)
-
-type newSUTConfig struct {
-	skipGlobalLogger bool
-}
-
-// withoutGlobalLogger skips the global libevm logger setup in [newSUT].
-func withoutGlobalLogger() newSUTOption {
-	return func(c *newSUTConfig) { c.skipGlobalLogger = true }
-}
-
 // newSUT returns a new SUT. Any >= [logging.Error] on the logger will also
 // cancel the returned context, which is useful when waiting for blocks that
 // can never finish execution because of an error.
 func newSUT(tb testing.TB, hooks *saehookstest.Stub) (context.Context, SUT) {
- 	tb.Helper()
- 	saetest.EnableLibEVMTBLogger(tb)
+	tb.Helper()
+	saetest.EnableLibEVMTBLogger(tb)
 	logger := saetest.NewTBLogger(tb, logging.Warn)
 	ctx := logger.CancelOnError(tb.Context())
 
@@ -652,13 +642,13 @@ func FuzzOpCodes(f *testing.F) {
 	// Although it's tempting to run multiple `code` slices in a block, to
 	// amortise the fixed setup cost of the SUT, this stops the Go fuzzer from
 	// knowing about their independence, resulting in a lot of empty inputs.
-    // Use a terminal logger instead of EnableLibEVMTBLogger because fuzz sub-tests run in parallel and cannot safely set a global logger.
-log.SetDefault(log.NewLogger(log.NewTerminalHandlerWithLevel(os.Stderr,log.LevelError, true)))
-    
-    f.Fuzz(func(t *testing.T, code []byte) {
+	// Use a terminal logger instead of EnableLibEVMTBLogger because fuzz sub-tests run in parallel and cannot safely set a global logger.
+	log.SetDefault(log.NewLogger(log.NewTerminalHandlerWithLevel(os.Stderr, log.LevelError, true)))
+
+	f.Fuzz(func(t *testing.T, code []byte) {
 		t.Parallel() // for corpus in ./testdata/
 
-		_, sut := newSUT(t, defaultHooks(), withoutGlobalLogger())
+		_, sut := newSUT(t, defaultHooks())
 		tx := sut.wallet.SetNonceAndSign(t, 0, &types.LegacyTx{
 			To:       nil, // i.e. contract creation, resulting in `code` being executed
 			GasPrice: big.NewInt(1),
