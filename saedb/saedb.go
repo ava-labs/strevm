b74e70ceee54fe2c1efa1e1292e53b91e9fb7d5e
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-04-03 17:58:01 -0400

    chore: maintain compiler optimization

--- saedb/saedb.go
+++ saedb/saedb.go
@@ -34,14 +34,19 @@ func (c *Config) SetCommitIntervalForTesting(interval uint64) {
 
 // ShouldCommitTrieDB returns whether or not to commit the state trie to disk.
 func (c Config) ShouldCommitTrieDB(blockNum uint64) bool {
-	return blockNum%c.CommitIntervalOrDefault() == 0
+	if c.commitInterval == 0 {
+		return blockNum%CommitTrieDBEvery == 0
+	}
+	return blockNum%c.commitInterval == 0
 }
 
 // LastCommittedTrieDBHeight returns the largest value <= the argument at which
 // [Config.ShouldCommitTrieDB] would have returned true.
 func (c Config) LastCommittedTrieDBHeight(atOrBefore uint64) uint64 {
-	interval := c.CommitIntervalOrDefault()
-	return atOrBefore / interval * interval
+	if c.commitInterval == 0 {
+		return atOrBefore / CommitTrieDBEvery * CommitTrieDBEvery
+	}
+	return atOrBefore / c.commitInterval * c.commitInterval
 }
 
 // A StateDBOpener opens a [state.StateDB] at the given root.
