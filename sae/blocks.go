5f73a1d60768d61d3c795aec341ac09a084a2c35
Stephen Buttolph <stephen@avalabs.org>
2026-04-08 13:17:55 -0400

    nit format

--- sae/blocks.go
+++ sae/blocks.go
@@ -116,11 +116,10 @@ var (
 	errSettledHeightMismatch = errors.New("settled height mismatch")
 )
 
-// verifyWhenBootstrapping skips verification in its entirety.
-//
-// During bootstrapping, blocks are verified by their hash in the consensus
-// engine. This support hooks, such as Coreth and Subnet-EVM, that are unable to
-// fully verify blocks during bootstrapping.
+// verifyWhenBootstrapping skips verification in its entirety. It is expected
+// blocks to be verified by their hash in the bootstrapping engine. This
+// supports hooks, such as Coreth and Subnet-EVM, that are unable to fully
+// verify blocks during bootstrapping.
 func (vm *VM) verifyWhenBootstrapping(b, parent *blocks.Block) error {
 	header := b.Header()
 	lastSettled, err := lastToSettle(vm.hooks, header, parent, vm.config.Now(), vm.log())
